822f29e1b24395afa9d4352d2e4623f68c138a50
yovi <yoviagustian94@gmail.com>
2020-04-28 04:39:58 +0700

    get all wishlist
    read update delete belum

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -2,12 +2,28 @@
 declare(strict_types=1);
 
 namespace App\Controllers;
+use App\Models\Wishlists;
+use App\Models\Books;
+
 class ProfileController extends ControllerBase
 {
 
     public function indexAction()
     {
+        $user = $this->session->get('auth');
+        $id = $user['userId'];
 
+        $books = array();
+        $wishlists = Wishlists::find("userId = '$id'");
+        
+        foreach ( $wishlists as $wishlist)
+        {
+            $tmp = Books::findfirst("bukuId = '$wishlist->bukuId'");
+            array_push($books, $tmp);
+        }
+
+        $this->view->books = $books;
+        
     }
 
 }
