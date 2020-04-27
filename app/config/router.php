21134d55227d1c7ea4b00d7ddd61768ddbbc780d
bagasys <37441075+bagasys@users.noreply.github.com>
2020-04-27 12:52:04 +0700

    Update router.php

--- app/config/router.php
+++ app/config/router.php
@@ -11,17 +11,7 @@ $router->removeExtraSlashes(true);
 
 
 
-$router->add('/katalog/:params', [
-    'namespace' => 'App\Controllers',
-    'controller' =>  'katalog',
-    'action' => 'detail',
-    'params' =>  1,
-]);
 
-$router->add('/katalog', [
-    'namespace' => 'App\Controllers',
-    'controller' =>  'katalog',
-]);
 
 
 
@@ -34,49 +24,33 @@ $router->add('/', [
 ]);
 
 
+$router->add('/:controller/:action', [
+    'namespace' => 'App\Controllers',
+    'controller' =>  1,
+    'action' =>  2,
+]);
+
+$router->add('/:controller', [
+    'namespace' => 'App\Controllers',
+    'controller' =>  1,
+    'action' =>  'index'
+]);
+
+$router->add('/:controller/:params', [
+    'namespace' => 'App\Controllers',
+    'controller' =>  1,
+    'action' =>  'index',
+    'params' => 2
+]);
 
 
 
-
-
-
-
-// $router->add('/admin/edit-buku/:params', [
-//     'namespace' => 'App\Controllers',
-//     'controller' =>  'admin',
-//     'action' =>  'editbuku',
-//     'params' =>  1,
-// ]);
-
-
-
-// $router->add('/:controller/:action', [
-//     'namespace' => 'App\Controllers',
-//     'controller' =>  1,
-//     'action' =>  2,
-// ]);
-
-// $router->add('/:controller', [
-//     'namespace' => 'App\Controllers',
-//     'controller' =>  1,
-//     'action' =>  'index'
-// ]);
-
-// $router->add('/:controller/:params', [
-//     'namespace' => 'App\Controllers',
-//     'controller' =>  1,
-//     'action' =>  'index',
-//     'params' => 2
-// ]);
-
-
-
-// $router->add('/:controller/:action/:params', [
-//     'namespace' => 'App\Controllers',
-//     'controller' =>  1,
-//     'action' =>  2,
-//     'params' =>  3,
-// ]);
+$router->add('/:controller/:action/:params', [
+    'namespace' => 'App\Controllers',
+    'controller' =>  1,
+    'action' =>  2,
+    'params' =>  3,
+]);
 
 // $router->notFound([
 //     'namespace' => 'App\Controllers',
